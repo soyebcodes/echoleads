e7644f93c156be60ce3ec1ac6ce635a814e73cc5
Soyeb Islam <soyeb.islam117@gmail.com>
2026-03-19 20:34:43 +0600

    feat: Introduce monorepo and initial shadcn component

---
+++ apps/web/src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>EchoLeads</h1>
+      <Button>Button</Button>
+    </main>
+  );
+}
